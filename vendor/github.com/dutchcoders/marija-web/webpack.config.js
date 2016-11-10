810a6d0d5b787a2f93235d1beeddddc1b325453d
Remco <remco@dutchcoders.io>
2016-11-10 20:13:37 +0100

    bumped latest frontend

--- vendor/github.com/dutchcoders/marija-web/webpack.config.js
+++ vendor/github.com/dutchcoders/marija-web/webpack.config.js
@@ -22,11 +22,11 @@ module.exports = {
     },
     module: {
         loaders: [
-            {
+            /*{
                 test: /\.js$/,
                 loaders: ["babel-loader", "eslint-loader"],
                 exclude: /node_modules/
-            },
+            },*/
             {
                 test: /.js?$/,
                 loader: 'babel-loader',
