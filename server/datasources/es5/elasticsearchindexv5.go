9205ae39b1e894f6cf64413d2549b19c6d6d6951
Remco Verhoef <remco@dutchcoders.io>
2018-07-12 10:34:37 +0200

    disable highlight results from elasticsearch
    * highlighting caused issues with returning different results, and
    following different ids

--- server/datasources/es5/elasticsearchindexv5.go
+++ server/datasources/es5/elasticsearchindexv5.go
@@ -127,16 +127,18 @@ func (i *Elasticsearch) Search(ctx context.Context, so datasources.SearchOptions
 		defer close(itemCh)
 		defer close(errorCh)
 
-		hl := elastic.NewHighlight()
-		hl = hl.Fields(elastic.NewHighlighterField("*").RequireFieldMatch(false).NumOfFragments(0))
-		hl = hl.PreTags("<em>").PostTags("</em>")
+		/*
+			hl := elastic.NewHighlight()
+			hl = hl.Fields(elastic.NewHighlighterField("*").RequireFieldMatch(false).NumOfFragments(0))
+			hl = hl.PreTags("<em>").PostTags("</em>")
+		*/
 
 		q := elastic.NewQueryStringQuery(so.Query)
 
 		src := elastic.NewSearchSource().
 			Query(q).
 			FetchSource(false).
-			Highlight(hl).
+			/*Highlight(hl).*/
 			FetchSource(true).
 			From(so.From).
 			Size(100)
@@ -187,9 +189,11 @@ func (i *Elasticsearch) Search(ctx context.Context, so datasources.SearchOptions
 
 				fields = flattenFields("", fields)
 
-				for key, val := range hit.Fields {
-					fields[key] = val
-				}
+				/*
+					for key, val := range hit.Fields {
+						fields[key] = val
+					}
+				*/
 
 				itemCh <- datasources.Item{
 					ID:     hit.Id,
