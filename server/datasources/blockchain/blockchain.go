f205cbf1446b45ce497fc37343a4670ae726ae14
Remco Verhoef <remco@dutchcoders.io>
2018-07-15 14:49:43 +0200

    datasources: blockchain: improved blockchain analysis
    * create different items for both in- and outputs
    * sum inputs

--- server/datasources/blockchain/blockchain.go
+++ server/datasources/blockchain/blockchain.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"fmt"
 	"time"
@@ -58,47 +59,110 @@ func (b *BTC) Search(ctx context.Context, so datasources.SearchOptions) datasour
 	errorCh := make(chan error)
 
 	go func() {
-		address := &blockchain.Address{Address: so.Query}
-		if err := b.client.Request(address); err != nil {
-			errorCh <- err
-			return
+		defer close(itemCh)
+		defer close(errorCh)
+
+		qry := strings.Replace(so.Query, "\"", "", -1)
+
+		sendTx := func(tx blockchain.Transaction) {
+			inputs := []string{}
+			sumOfInput := float64(0)
+
+			for _, input := range tx.Inputs {
+				inputs = append(inputs, input.PrevOut.Address)
+				sumOfInput += float64(input.PrevOut.Value)
+			}
+
+			fields := map[string]interface{}{
+				"relayed_by":   tx.RelayedBy,
+				"label":        fmt.Sprintf("sum(in): %f", sumOfInput/100000000),
+				"address":      inputs,
+				"type":         "input",
+				"hash":         tx.Hash,
+				"size":         tx.Size,
+				"block_height": tx.BlockHeight,
+				"version":      tx.Version,
+				"date":         time.Unix(tx.Time, 0),
+				"input":        inputs,
+				"value":        fmt.Sprintf("%f", sumOfInput/100000000),
+			}
+
+			item := datasources.Item{
+				ID:     fmt.Sprintf("input.%s", tx.Hash),
+				Fields: fields,
+			}
+
+			select {
+			case itemCh <- item:
+			case <-ctx.Done():
+				return
+			}
+
+			for _, output := range tx.Outputs {
+				fields := map[string]interface{}{
+					"relayed_by":       tx.RelayedBy,
+					"label":            fmt.Sprintf("out (%d): %f", output.Number, float64(output.Value)/100000000),
+					"address":          []string{output.Address},
+					"number":           output.Number,
+					"type":             "output",
+					"tx_index":         output.TransactionIndex,
+					"size":             tx.Size,
+					"block_height":     tx.BlockHeight,
+					"version":          tx.Version,
+					"hash":             tx.Hash,
+					"date":             time.Unix(tx.Time, 0),
+					"output":           []string{output.Address},
+					"value":            fmt.Sprintf("%f", float64(output.Value)/100000000),
+					"address_tag":      output.AddressTag,
+					"address_tag_link": output.AddressTagLink,
+				}
+
+				item := datasources.Item{
+					ID:     fmt.Sprintf("output.%s.%s.%d", tx.Hash, output.Address, output.TransactionIndex),
+					Fields: fields,
+				}
+
+				select {
+				case itemCh <- item:
+				case <-ctx.Done():
+					return
+				}
+			}
 		}
 
-		for {
+		if len(qry) == 64 {
+			tx := blockchain.Transaction{
+				Hash: qry,
+			}
 
-			tx, err := address.NextTransaction()
-			if err == blockchain.IterDone {
-				break
-			} else if err != nil {
+			if err := b.client.Request(&tx); err != nil {
 				errorCh <- err
 				return
 			}
 
-			for _, input := range tx.Inputs {
-				for _, output := range tx.Outputs {
-					fields := map[string]interface{}{
-						"relayed_by":       tx.RelayedBy,
-						"date":             time.Unix(tx.Time, 0),
-						"input":            input.PrevOut.Address,
-						"output":           output.Address,
-						"value":            input.PrevOut.Value,
-						"address_tag":      output.AddressTag,
-						"address_tag_link": output.AddressTagLink,
-					}
-
-					item := datasources.Item{
-						ID:     fmt.Sprintf("%s%s%s", tx.Hash, input.PrevOut.Address, output.Address),
-						Fields: fields,
-					}
-
-					select {
-					case itemCh <- item:
-					case <-ctx.Done():
-						return
-					}
-				}
+			sendTx(tx)
+		} else if len(qry) == 34 {
+			item := blockchain.Address{Address: qry}
+			if err := b.client.Request(&item); err != nil {
+				errorCh <- err
+				return
 			}
+
+			for {
+				tx, err := item.NextTransaction()
+				if err == blockchain.IterDone {
+					break
+				} else if err != nil {
+					errorCh <- err
+					return
+				}
+
+				sendTx(tx)
+			}
+
+			return
 		}
+
 	}()
 
 	return datasources.NewSearchResponse(
@@ -109,7 +173,7 @@ func (b *BTC) Search(ctx context.Context, so datasources.SearchOptions) datasour
 
 func (i *BTC) GetFields(context.Context) (fields []datasources.Field, err error) {
 	fields = append(fields, datasources.Field{
-		Path: "id",
+		Path: "number",
 		Type: "string",
 	})
 
@@ -121,6 +185,41 @@ func (i *BTC) GetFields(context.Context) (fields []datasources.Field, err error)
 	fields = append(fields, datasources.Field{
 		Path: "date",
 		Type: "date",
+	})
+
+	fields = append(fields, datasources.Field{
+		Path: "label",
+		Type: "string",
+	})
+
+	fields = append(fields, datasources.Field{
+		Path: "hash",
+		Type: "string",
+	})
+
+	fields = append(fields, datasources.Field{
+		Path: "size",
+		Type: "string",
+	})
+
+	fields = append(fields, datasources.Field{
+		Path: "block_height",
+		Type: "string",
+	})
+
+	fields = append(fields, datasources.Field{
+		Path: "version",
+		Type: "string",
+	})
+
+	fields = append(fields, datasources.Field{
+		Path: "tx_index",
+		Type: "string",
+	})
+
+	fields = append(fields, datasources.Field{
+		Path: "address",
+		Type: "string",
 	})
 
 	fields = append(fields, datasources.Field{
@@ -144,7 +243,7 @@ func (i *BTC) GetFields(context.Context) (fields []datasources.Field, err error)
 
 	fields = append(fields, datasources.Field{
 		Path: "value",
-		Type: "int",
+		Type: "string",
 	})
 
 	return
